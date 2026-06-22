36d0420e76b94cd073bcbc0e17f63d3d82a41780
David Sinclair <david_sinclair@apple.com>
2026-06-22 09:02:40 -0700

    rsvp start

--- src/components/Rsvp.tsx
+++ src/components/Rsvp.tsx
@@ -15,8 +15,8 @@ import { useMemo, useState } from "react";
 import MiniSearch from "minisearch";
 
 export default function Rsvp({ data }: { data: string[][] }) {
-  const miniSearch = useMemo(() => {
-    const items = data.map((item, i) => ({
+  const { miniSearch, items } = useMemo(() => {
+    const _items = data.map((item, i) => ({
       id: i,
       names: item.filter((t) => !!t),
     }));
@@ -24,8 +24,8 @@ export default function Rsvp({ data }: { data: string[][] }) {
       fields: ["names"],
       storeFields: ["names", "id"],
     });
-    _miniSearch.addAll(items);
-    return _miniSearch;
+    _miniSearch.addAll(_items);
+    return { miniSearch: _miniSearch, items: _items };
   }, [data]);
   const searchForm = useForm(getSearchForm());
   const [results, setResults] = useState<null | any[]>(null);
@@ -34,19 +34,32 @@ export default function Rsvp({ data }: { data: string[][] }) {
     const filtered = results.filter((result) => result.score >= 5);
     setResults(filtered);
   });
+  const [record, setRecord] = useState<null | any>(null);
   const onSelect = (id: number) => {
-    console.log({ id });
+    const found = results?.find((item) => item.id === id);
+
+    if (found) {
+      setRecord(record);
+    }
   };
 
   return (
     <div className="flex flex-col gap-10 items-center">
       <form onSubmit={onSearch} className="flex flex-col gap-2.5">
-        <p>Please enter your first and last name.</p>
+        <p>Please enter your first and last name</p>
         <TextInput
           aria-label="First and Last Name"
           key={searchForm.key("name")}
           autoFocus
           {...searchForm.getInputProps("name")}
+          inputContainer={(children) => (
+            <>
+              {children}
+              <Text size="xs" mt={2.5}>
+                Ex. Sarah Fortune (not The Fortune Family)
+              </Text>
+            </>
+          )}
         />
         <Button fullWidth type="submit">
           Search
@@ -57,24 +70,24 @@ export default function Rsvp({ data }: { data: string[][] }) {
           <p>No results found, please try again.</p>
         )}
         {results !== null && results?.length > 0 && (
-          <ul className="w-2xs [&>*:nth-child(n+2)]:pt-5">
+          <ul className="w-2xs [&>*:nth-child(n+2)]:mt-5">
             {results.map((item) => (
-              <li key={item.id}>
-                <Paper shadow="sm" p="lg">
-                  <div className="flex justify-between items-center">
-                    <div>
-                      {item.names.map((name: string) => (
-                        <Text key={name}>{name}</Text>
-                      ))}
-                    </div>
-                    <Button onClick={() => onSelect(item.id)}>Select</Button>
-                  </div>
-                </Paper>
+              <li
+                key={item.id}
+                className="flex justify-between items-center border border-gray-300 p-5 rounded-lg"
+              >
+                <div>
+                  {item.names.map((name: string) => (
+                    <Text key={name}>{name}</Text>
+                  ))}
+                </div>
+                <Button onClick={() => onSelect(item.id)}>Select</Button>
               </li>
             ))}
           </ul>
         )}
       </div>
+      {record && <div>{record.names.join(", ")}</div>}
     </div>
   );
 }
